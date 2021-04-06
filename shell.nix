1685da84196204b7a144c51085b17dad09860836
TechnoElf <technoelf@undertheprinter.com>
2021-04-06 12:31:06 +0200

    Cleanup

--- shell.nix
+++ shell.nix
@@ -3,15 +3,8 @@ let
   pkgs = import <nixpkgs> {
     overlays = [ moz_overlay ];
   };
-  cross_pkgs = import <nixpkgs> {
-    overlays = [ moz_overlay ];
-    crossSystem = {
-      config = "aarch64-unknown-linux-musl";
-    };
-  };
 in with pkgs; pkgs.mkShell {
   buildInputs = [
-    (rustChannelOfTargets "nightly" "2021-03-13" [ "x86_64-unknown-linux-gnu" "aarch64-unknown-linux-musl" ])
-    cross_pkgs.stdenv.cc
+    (rustChannelOfTargets "nightly" "2021-03-13" [ "x86_64-unknown-linux-gnu" ])
   ];
 }
